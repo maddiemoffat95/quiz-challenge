d5e7ad3214c40701deab279919038746024c6fca
maddiemoffat <maddiemoffat@hotmail.com>
2020-10-23 19:36:27 -0600

    re working timer and shome score funtion

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,86 +1,175 @@
-var quiz =[
-     quesstions={
+var questions = [
+    {
         text: "Which of the following is correct about JavaScript?",
-        choices =[
-        "JavaScript is a lightweight, interpreted programming language.",
-        "JavaScript has object-oriented capabilities that allows you to build interactivity into otherwise static HTML pages.",
-        "The general-purpose core of the language has been embedded in Netscape, Internet Explorer, and other web browsers.",
-        "All of the above.",
+        choices: [
+            "JavaScript is a lightweight, interpreted programming language.",
+            "JavaScript has object-oriented capabilities that allows you to build interactivity into otherwise static HTML pages.",
+            "The general-purpose core of the language has been embedded in Netscape, Internet Explorer, and other web browsers.",
+            "All of the above."
         ],
-        answer : "All of the above.",
+        answer: "All of the above."
+    },
 
+    {
         text: "Which of the following is the correct syntax to redirect a url using JavaScript?",
-        choices = [
+        choices: [
             "document.location='http://www.newlocation.com';",
             "browser.location='http://www.newlocation.com';",
             "navigator.location='http://www.newlocation.com';",
-            "window.location='http://www.newlocation.com';",
+            "window.location='http://www.newlocation.com';"
         ],
-        answer: "window.location='http://www.newlocation.com';",
+        answer: "window.location='http://www.newlocation.com';"
+    },
 
+    {
         text: "Which built-in method combines the text of two strings and returns a new string?",
-        choices =[
+        choices: [
             "append()",
             "concat()",
             "attach()",
-            "None of the above.",
+            "None of the above."
         ],
-        answer : "concat()",
+        answer: "concat()"
+    },
 
-        text : "Which of the following function of Number object forces a number to display in exponential notation?",
-        choices = [
+    {
+        text: "Which of the following function of Number object forces a number to display in exponential notation?",
+        choices: [
             "toExponential()",
             "toFixed()",
             "toPrecision()",
-            "toLocaleString()",
+            "toLocaleString()"
         ],
-        answer: "toExponential()",
+        answer: "toExponential()"
+    },
 
+    {
         text: "Which of the following function of String object returns the character at the specified index?",
-        choices =[
+        choices: [
             "charAt()",
             "charCodeAt()",
             "concat()",
-            "indexOf()",
+            "indexOf()"
         ],
-        answer: "charAt()",
+        answer: "charAt()"
     }
 ]
 
-let start= document.getElementById("start-button");
+let start = document.getElementById("start-button");
+let startOver = document.getElementById("start-over");
+let timeLeft = 75;
+let timer = document.getElementById("timer");
+let runningQ = 0;
+let curQ = questions[runningQ];
+let questionDiv = document.getElementById("question-div");
+let lastQ = questions[questions.length - 1];
+let timerId;
+let initials;
+let submit = document.getElementById("submit-button");
 
 
-
-
-
-function displayQuestion (){
-    questions.forEach(choices => {
+function displayQuestion(curQ) {
+    document.getElementById("question-div").style.display = "block";
+    document.getElementById("start-button").style.display = "none";
+    document.getElementById("header").style.display = "none";
+    questionDiv.innerHTML = "<p>" + curQ.text + "</p>";
+    curQ.choices.forEach(choice => {
+        var button = document.createElement("button");
+        button.textContent = choice
+        questionDiv.append(button)
     });
-};
-
-function checkAnswer (){
-
-};
-
-function startTimer (){
+    questionDiv.childNodes[1].addEventListener("click", function () { checkAnswer(questionDiv.childNodes[1].outerText) });
+    questionDiv.childNodes[2].addEventListener("click", function () { checkAnswer(questionDiv.childNodes[2].outerText) });
+    questionDiv.childNodes[3].addEventListener("click", function () { checkAnswer(questionDiv.childNodes[3].outerText) });
+    questionDiv.childNodes[4].addEventListener("click", function () { checkAnswer(questionDiv.childNodes[4].outerText) });
 
 };
 
-function endTimer (){
+function checkAnswer(answer) {
 
+    if (answer == curQ.answer) {
+        document.getElementById("answer-div").style.display = "none";
+        if (answer == lastQ.answer) {
+            endQuiz();
+        }
+        else {
+            runningQ = runningQ + 1;
+            curQ = questions[runningQ];
+            displayQuestion(curQ);
+        }
+    }
+    else {
+        document.getElementById("answer-div").style.display = "block";
+    }
 };
 
-function showScore (){
+function startTimer() {
+    if (timeLeft == 75) {
+        timerId = setInterval(startTimer, 1000);
+    }
 
+    if (timeLeft == 0) {
+        clearTimeout(timerId);
+
+    } else {
+        timer.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+    }
+}
+
+function showScore() {
+    document.getElementById("timer").style.display = "none";
+    document.getElementById("question-div").style.display = "none";
+    document.getElementById("answer-div").style.display = "none";
+    document.getElementById("score").style.display = "block";
+    document.getElementById("score-div").style.display = "block";
+    document.getElementById("score-div").innerHTML = "<p>" + "Congratulations! Your Score is: " + timeLeft + "</p>";
+    submit.addEventListener("click", showHighScore);
 };
 
-function showHighScore (){
+function showHighScore() {
+    document.getElementById("score").style.display = "none";
+    document.getElementById("high-score").style.display = "block";
+    initials = document.getElementById("initial-box").value
+    let userScore = {
+        "user": initials,
+        "score": timeLeft
+    };
 
+    localStorage.setItem("highScore", JSON.stringify(userScore));
+    var retrievedData = localStorage.getItem("highScore");
+
+    document.getElementById("high-score").innerHTML = "High Score" + "<p>" + retrievedData + "</p>"
+    startOver.addEventListener("click", secondStart);
 };
 
-function startQuiz (){
+// I need to create a start over button and have it call a fucntion that starts everything over instead of the page reloading after showHighScore and also have the button terminate a while loop
+
+function endQuiz() {
+    document.getElementById("question-div").style.display = "none";
+    document.getElementById("answer-div").style.display = "none";
+    clearTimeout(timerId);
+    showScore();
+}
+
+function startQuiz(e) {
+    e.preventDefault();
     startTimer();
-    displayQuestion();
+    displayQuestion(curQ);
 };
 
-start.addEventListener("click", startQuiz)
+function secondStart() {
+    document.getElementById("header").style.display = "block";
+    document.getElementById("start-button").style.display = "block";
+    document.getElementById("question-div").style.display = "none";
+    document.getElementById("answer-div").style.display = "none";
+    document.getElementById("score").style.display = "none";
+    document.getElementById("score-div").style.display = "none";
+};
+
+document.getElementById("question-div").style.display = "none";
+document.getElementById("answer-div").style.display = "none";
+document.getElementById("score").style.display = "none";
+document.getElementById("score-div").style.display = "none";
+
+start.addEventListener("click", startQuiz);
